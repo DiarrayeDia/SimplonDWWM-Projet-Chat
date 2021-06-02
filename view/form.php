cd63f5f337631afe8cb21ea41fd7773c4c1345a5
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-02 14:03:28 +0200

    updated view files

--- view/form.php
+++ view/form.php
@@ -1,16 +1,19 @@
 <!-- Saisie de l'utilisateur : Pseudo + Commentaire + Submit-->
 <form>
-    <div class="form-group">
-        <label class="col-form-label mt-4" for="input">Entrez votre pseudo</label>
-        <input type="text" class="form-control" placeholder="Entrez votre pseudo" id="input">
-    </div>
+    <fieldset>
+        <div class="row">
+            <div class="form-group col-4">
+                <label for="pseudo_fe" class="form-label mt-4" hidden>Pseudo</label>
+                <input type="text" class="form-control" id="pseudo_fe" aria-describedby="pseudo" placeholder="Entrez votre pseudo">
+            </div>
 
-    <div class="form-group">
-        <label for="text" class="form-label mt-4">Zone de commentaire</label>
-        <textarea class="form-control" id="text" rows="10"></textarea>
-    </div>
+            <div class="form-group col-8">
+                <label for="message_fe" class="form-label mt-4" hidden>Message</label>
+                <textarea class="form-control" id="message_fe" rows="3" placeholder="Entrez votre message"></textarea>
+            </div>
+        </div>
 
-    </fieldset>
-    <button type="submit" class="btn btn-success">Submit</button>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+
     </fieldset>
 </form>
