6dacdf41d93d6f4cd5c6ba9e7ffa78161fefee0b
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-02 13:43:17 +0200

    updated chat file

--- view/chat.php
+++ view/chat.php
@@ -2,15 +2,22 @@
 <table class="table table-hover">
     <thead>
         <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Auteur</th>
-            <th scope="col">Commentaire</th>
+            <th scope="col" class="col-2" hidden>date</th>
+            <th scope="col" class="col-2" hidden>pseudo</th>
+            <th scope="col" class="col-8" hidden>message</th>
         </tr>
     </thead>
-    <tr class="table-dark">
-        <td>01/06/2021</td>
-        <td>Dia</td>
-        <td>Bienvenue en Juin! Enfin l'été !!</td>
-    </tr>
+    <tbody>
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+        ?>
+            <tr class="table-light">
+                <td class="col-2">01/06/2021</td>
+                <td class="col-2">camile</td>
+                <td class="col-8">Salut !</td>
+            </tr>
+        <?php
+        }
+        ?>
     </tbody>
 </table>
