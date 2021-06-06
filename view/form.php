86371e804a14cd86cc06079710b3e8aa23ef80dd
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-06 15:38:40 +0200

    added value to pseudo text area

--- view/form.php
+++ view/form.php
@@ -1,10 +1,10 @@
 <!-- Saisie de l'utilisateur : Pseudo + Commentaire + Submit-->
-<form>
+<form method="POST" action="index.php">
     <fieldset>
         <div class="row">
             <div class="form-group col-4">
                 <label for="pseudo_fe" class="form-label mt-4" hidden>Pseudo</label>
-                <input type="text" class="form-control" id="pseudo_fe" aria-describedby="pseudo" placeholder="Entrez votre pseudo">
+                <input type="text" name="pseudo" value="<?= isset($_POST['pseudo']) ? $_POST['pseudo'] : '' ?>" class="form-control" id="pseudo_fe" aria-describedby="pseudo" placeholder="Entrez votre pseudo">
             </div>
 
             <div class="form-group col-8">
